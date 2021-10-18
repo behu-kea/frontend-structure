5cd9e2446020c95284abe0e46dd4b58ed0e30a3e
Benjamin Hughes <behu@kea.dk>
2021-10-18 11:27:32 +0200

    asd

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -3,4 +3,6 @@ console.log("in search");
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-console.log(params);
+if ("name" in params) {
+  document.querySelector("h1").innerText = `${params.name}'s profile page`;
+}
