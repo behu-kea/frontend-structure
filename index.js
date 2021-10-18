89e6fccf96a402b4a10a6cd42882d756e178a373
Benjamin Hughes <behu@kea.dk>
2021-10-18 11:50:07 +0200

    asd

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ console.log("in index.js");
 function addDataToPage() {
   const lastHrefCharacter =
     window.location.href[window.location.href.length - 1];
+  // So the href might have a backslash (/) in the url or it might not
+  // fx https://behu-kea.github.io/frontend-structure/ but with localhost it looks like this http://localhost:5500
+  // This we need to handle
   const href =
-    lastHrefCharacter !== "/" ? `window.location.href/` : window.location.href;
+    lastHrefCharacter !== "/"
+      ? `${window.location.href}/`
+      : window.location.href;
   // First create a new url with the page you want to navigate to
   const url = new URL(`${href}pages/profile/profile.html`);
 
