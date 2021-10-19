e5a092294dce49a154a11af5001c653ad08dbdf7
Benjamin Hughes <behu@kea.dk>
2021-10-19 14:25:33 +0200

    asd

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 console.log("in index.js");
 
 function addDataToPage() {
-  const lastHrefCharacter =
-    window.location.href[window.location.href.length - 1];
-  // So the href might have a backslash (/) in the url or it might not
-  // fx https://behu-kea.github.io/frontend-structure/ but with localhost it looks like this http://localhost:5500
-  // This we need to handle
-  const href =
-    lastHrefCharacter !== "/"
-      ? `${window.location.href}/`
-      : window.location.href;
   // First create a new url with the page you want to navigate to
-  const url = new URL(`${href}pages/profile/profile.html`);
+  const url = new URL(`${getHref()}pages/profile/profile.html`);
 
   // Now add the query parameter
   url.searchParams.append("name", "benjamin");
@@ -21,3 +12,17 @@ function addDataToPage() {
 }
 
 addDataToPage();
+
+// So the href might have a backslash (/) in the url or it might not
+// fx https://behu-kea.github.io/frontend-structure/ but with localhost it looks like this http://localhost:5500
+// This we need to handle
+function getHref() {
+  const lastHrefCharacter =
+    window.location.href[window.location.href.length - 1];
+  const href =
+    lastHrefCharacter !== "/"
+      ? `${window.location.href}/`
+      : window.location.href;
+
+  return href;
+}
